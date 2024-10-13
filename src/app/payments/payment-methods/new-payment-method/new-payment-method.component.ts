f17690785a4f6a537190d290fb26f456bf38aa33
Milad Abdollahi <abd.milad@gmail.com>
2024-10-13 22:54:42 +0330

    worked on add payment method

--- src/app/payments/payment-methods/new-payment-method/new-payment-method.component.ts
+++ src/app/payments/payment-methods/new-payment-method/new-payment-method.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PaymentMethodCreateDto } from '../../../shared/models/dtos/payment-methos-create-dto.model';
+import { PaymentMethodService } from '../../../shared/services/payment-method.service';
 
 @Component({
     selector: 'app-new-payment-method',
@@ -9,8 +11,26 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     styleUrl: './new-payment-method.component.css',
 })
 export class NewPaymentMethodComponent {
+    private paymentMethodService = inject(PaymentMethodService);
+
     public newPaymentMethodForm = new FormGroup({
-        methodName: new FormControl<string | undefined>(undefined),
+        methodName: new FormControl<string | undefined>(undefined, Validators.required),
     });
-    
+
+    onSave() {
+        console.log(this.newPaymentMethodForm);
+
+        const paymentMethodCreateDto: PaymentMethodCreateDto = {
+            metodName: this.newPaymentMethodForm.value.methodName!,
+        };
+        console.log(paymentMethodCreateDto);
+        this.paymentMethodService.CreatePaymentMethod(paymentMethodCreateDto).subscribe({
+            next: (resData) => {
+                console.log(resData);
+            },
+            error: (err) => {
+                window.alert(err);
+            },
+        });
+    }
 }
